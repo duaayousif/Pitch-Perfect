549efc7ca894ed129abcd73343b3ba44dc2d2842
duaayousif <dyaabdel@uwaterloo.ca>
2015-05-23 17:35:34 -0400

    Initial Commit

---
+++ Pitch PerfTests/Pitch_PerfTests.swift
@@ -0,0 +1,36 @@
+//
+//  Pitch_PerfTests.swift
+//  Pitch PerfTests
+//
+//  Created by Duaa Abdelgadir on 2015-05-23.
+//  Copyright (c) 2015 Duaa Abdelgadir. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Pitch_PerfTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
